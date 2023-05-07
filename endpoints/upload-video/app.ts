07551d26a4649ca2763a8becb43fbbb8483aec8d
Andrew Vitale <vitale232@gmail.com>
2023-05-06 21:40:13 -0400

    feat: add upload-video endpoint

---
+++ endpoints/upload-video/app.ts
@@ -0,0 +1,56 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
+
+/**
+ *
+ * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+ * @param {Object} event - API Gateway Lambda Proxy Input Format
+ *
+ * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+ * @returns {Object} object - API Gateway Lambda Proxy Output Format
+ *
+ */
+
+export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    console.log(`Starting at: ${new Date()}`);
+    try {
+        console.log({ event });
+        const contentType = event.headers?.['Content-Type'];
+        console.log({ contentType });
+        const fileName =
+            event.queryStringParameters?.['fileName'] ??
+            event.queryStringParameters?.['filename'] ??
+            `vid_uploaded_${new Date().getTime()}.mp4`;
+        const videoBase64 = event.body as string;
+
+        if (!videoBase64) {
+            return {
+                statusCode: 400,
+                body: JSON.stringify({ message: 'No video data provided' }),
+            };
+        }
+        const client = new S3Client({});
+        const command = new PutObjectCommand({
+            Bucket: 'radar-speed-sign',
+            Key: fileName,
+            Body: Buffer.from(videoBase64, 'base64'),
+            ContentType: contentType ?? 'vido/mp4',
+        });
+        console.log({ command });
+        await client.send(command);
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                message: `Successfully uploaded ${fileName}`,
+            }),
+        };
+    } catch (err) {
+        console.log('an error occurred:', err);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({
+                message: `Error: ${err}`,
+            }),
+        };
+    }
+};
